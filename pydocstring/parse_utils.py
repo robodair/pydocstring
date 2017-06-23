8beae19c9e02b6ee14edc0f5f13a840f5937cd9b
Alisdair Robertson <alisdair.w.robertson@gmail.com>
2017-06-23 23:04:53 +1000

    parse_utils module for function and class parsing utilities

---
+++ pydocstring/parse_utils.py
@@ -0,0 +1,80 @@
+"""Utilities for dealing with functions as blocks of text e.g. parsing parameters, return values"""
+from __future__ import print_function
+import re
+import textwrap
+from collections import OrderedDict
+
+# TODO: parse_function_parameters method
+# def parse_function_parameters():
+#     """Parse function parameters into an OrderedDict of Parameters
+
+#     Args:
+#         s (str): everything in the parenthesis of a function
+#             declaration
+#         ret_annotation (str): return annotation if any
+#         default_type (str): default type text
+#         default_description (str): default text
+#         optional_tag (str): tag included with type for kwargs when
+#             they are created
+
+#     Returns:
+#         OrderedDict containing Parameter instances
+#     """
+
+
+def parse_return_keyword(text):
+    """Scan a function's code to look for how it returns, and what follows the return or yield
+    You should pass the block of code as returned by `Document`'s `get_block()` method
+
+    Args:
+        text: The text of a function
+
+    Returns:
+        list of tuples: 'return' or 'yield' and the statement following it
+    """
+    return_re = re.compile(r"^\s*(return|yield)\s*(.*)", re.MULTILINE)
+    matches = set()
+    for match in return_re.finditer(text):
+        matches.add(match.groups())
+    return list(matches)
+
+
+def parse_function_exceptions(text):
+    """Scan a function looking for where it raises exceptions
+    Args:
+        text: The text of a function
+
+    Returns:
+        list of tuples: 'raise' and the Exception following it
+    """
+    execp_re = re.compile(r"^[^\S\n]*(raise)[^\S\n]+([^\s\(]+)", re.MULTILINE)
+    matches = set()
+    for match in execp_re.finditer(text):
+        matches.add(match.groups())
+    return list(matches)
+
+
+def parse_class_attributes(text):
+    """Scan a class for all of the attributes
+
+    Args:
+        text: The text of a function
+
+    Returns:
+        list: A list of the class attributes
+    """
+    dedent_text = textwrap.dedent(text)
+    # Determine the indentation used in the class
+    indentations = [line[:len(line) - len(line.lstrip())] for line in dedent_text.splitlines()]
+    indent = min([indent for indent in indentations if indent])
+    print("Indent:'" + indent + "'")
+    execp_re = re.compile(r"^(^{0}([A-Za-z0-9_]+)|^{1}self\.([A-Za-z0-9_]+))\s*=\s*(.*)"
+                          .format(indent, indent * 2), re.MULTILINE)
+    matches = OrderedDict()
+    for match in execp_re.finditer(dedent_text):
+        if match.group(2): # class variable
+            matches[match.group(2)] = match.group(4)
+        elif match.group(3): # instance variable
+            matches[match.group(3)] = match.group(4)
+    matches_list = [(key, value) for key, value in matches.iteritems()]
+    return matches_list
---
+++ pydocstring/tests/test_parse_utils.py
@@ -0,0 +1,168 @@
+"""Tests for function module"""
+
+import unittest
+from .. import parse_utils
+
+class TestFunctions(unittest.TestCase):
+    """Test functionality of the functions module"""
+
+    def test_return_kw_none(self):
+        """Test the response when there is no return keyword"""
+        source = \
+'''    def _handle_long_word(self, reversed_chunks, cur_line, cur_len, width):
+        """_handle_long_word(chunks : [string],
+                             cur_line : [string],
+                             cur_len : int, width : int)
+
+        Handle a chunk of text (most likely a word, not whitespace) that
+        is too long to fit in any line.
+        """
+        # Figure out when indent is larger than the specified width, and make
+        # sure at least one character is stripped off on every pass
+        if width < 1:
+            space_left = 1
+        else:
+            space_left = width - cur_len
+
+        # If we're allowed to break long words, then do so: put as much
+        # of the next chunk onto the current line as will fit.
+        if self.break_long_words:
+            cur_line.append(reversed_chunks[-1][:space_left])
+            reversed_chunks[-1] = reversed_chunks[-1][space_left:]
+
+        # Otherwise, we have to preserve the long word intact.  Only add
+        # it to the current line if there's nothing already there --
+        # that minimizes how much we violate the width constraint.
+        elif not cur_line:
+            cur_line.append(reversed_chunks.pop())
+
+        # If we're not allowed to break long words, and there's already
+        # text on the current line, do nothing.  Next time through the
+        # main loop of _wrap_chunks(), we'll wind up here again, but
+        # cur_len will be zero, so the next line will be entirely
+        # devoted to the long word that we can't handle right now.
+'''
+        result = parse_utils.parse_return_keyword(source)
+        self.assertEqual(result, [])
+
+    def test_return_kw_return(self):
+        """Test the response when there is a return keyword"""
+        source = \
+'''def fill(text, width=70, **kwargs):
+    """Fill a single paragraph of text, returning a new string.
+
+    Reformat the single paragraph in 'text' to fit in lines of no more
+    than 'width' columns, and return a new string containing the entire
+    wrapped paragraph.  As with wrap(), tabs are expanded and other
+    whitespace characters converted to space.  See TextWrapper class for
+    available keyword args to customize wrapping behaviour.
+    """
+    w = TextWrapper(width=width, **kwargs)
+    return w.fill(text)
+'''
+        result = parse_utils.parse_return_keyword(source)
+        self.assertEqual(result, [('return', 'w.fill(text)')])
+
+    def test_return_kw_yield(self):
+        """Test the response when there is a yield keyword"""
+        source = \
+'''def firstn(n):
+     num = 0
+     while num < n:
+         yield num
+         num += 1
+         yield num+7
+'''
+        result = parse_utils.parse_return_keyword(source)
+        self.assertEqual(result, [('yield', 'num'), ('yield', 'num+7')])
+
+    def test_parse_exceptions(self):
+        """Test that exceptions are found in functions"""
+        source = \
+'''def demo_bad_catch():
+    try:
+        raise ValueError('represents a hidden bug, do not catch this')
+        raise Exception('This is the exception you expect to handle')
+        raise ValueError('Another Value Error, but it's only shown once')
+    except Exception as error:
+        print('caught this error: ' + repr(error))
+'''
+        result = parse_utils.parse_function_exceptions(source)
+        self.assertEqual(result, [('raise', 'Exception'), ('raise', 'ValueError'),])
+
+
+    def test_parse_class_attributes_simple(self):
+        """Test class attributes are correctly garnered from a simple class"""
+        source = \
+'''class TestClass(object):
+    class_attr_1 = 0
+    class_attr_2 = 2
+    class_attr_override = 3
+
+    def __init__(self):
+        self.inst_attr_1 = 1
+        self.inst_attr_2 = 2
+        self.class_attr_override = 0
+'''
+        result = parse_utils.parse_class_attributes(source)
+        self.assertEqual(result, [('class_attr_1', '0'),
+                                  ('class_attr_2', '2'),
+                                  ('class_attr_override', '0'),
+                                  ('inst_attr_1', '1'),
+                                  ('inst_attr_2', '2')])
+
+    def test_parse_class_attributes_nested_indented(self):
+        """Test class attributes are correctly garnered from a simple class"""
+        source = \
+'''    class TestClass(object):
+        class_attr_1 = 0
+        class_attr_2 = 2
+        class_attr_override = 3
+
+        def __init__(self):
+            self.inst_attr_1 = 1
+            self.inst_attr_2 = 2
+            self.class_attr_override = 0
+
+        class ChildClass(object):
+            sub_class_attr = 0
+
+            def __init__(self):
+                self.inst_attr_1 = 1
+                self.inst_attr_2 = 2
+                self.class_attr_override = 0
+'''
+        result = parse_utils.parse_class_attributes(source)
+        self.assertEqual(result, [('class_attr_1', '0'),
+                                  ('class_attr_2', '2'),
+                                  ('class_attr_override', '0'),
+                                  ('inst_attr_1', '1'),
+                                  ('inst_attr_2', '2')])
+
+    def test_parse_class_attributes_ignored_nested_class(self):
+        """Test class attributes from a nested class are ignored"""
+        source = \
+'''class TestClass(object):
+    class_attr_1 = 0
+    class_attr_2 = 2
+    class_attr_override = 3
+
+    def __init__(self):
+        self.inst_attr_1 = 1
+        self.inst_attr_2 = 2
+        self.class_attr_override = 0
+
+    class ChildClass(object):
+        sub_class_attr = 0
+
+        def __init__(self):
+            self.inst_attr_1 = 1
+            self.inst_attr_2 = 2
+            self.class_attr_override = 0
+'''
+        result = parse_utils.parse_class_attributes(source)
+        self.assertEqual(result, [('class_attr_1', '0'),
+                                  ('class_attr_2', '2'),
+                                  ('class_attr_override', '0'),
+                                  ('inst_attr_1', '1'),
+                                  ('inst_attr_2', '2')])
