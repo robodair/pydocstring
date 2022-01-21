7f775892751cb20a456fa80ddf49b82eb19369ee
Benoit Coste <benoit.coste@protonmail.com>
2022-01-21 02:58:37 +0100

    Merge all formatter codes (#11)
    Instead of having one module for each style (google, numpy, reST), use a single
    module and provide the styling preferences through a dictionary.

--- pydocstring/__init__.py
+++ pydocstring/__init__.py
@@ -6,8 +6,59 @@ Top level API - all you need to integrate with an editor, just call
 __version__ = "0.2.1"
 
 import parso
-from parso.python.tree import search_ancestor, BaseNode
-from pydocstring import exc, formatters
+from parso.python.tree import BaseNode, search_ancestor
+
+import pydocstring.formatter
+from pydocstring import exc
+
+FORMATTER = {
+    "google": {
+        "start_args_block": "\n\nArgs:\n",
+        "param_placeholder": "    {0} ({1}): {2}\n",
+        "param_placeholder_args": "    *{0}: {1}\n",
+        "param_placeholder_kwargs": "    **{0}: {1}\n",
+        "start_return_block": "\n\nReturns:\n",
+        "return_placeholder": "    {0}: {1}\n",
+        "return_annotation_placeholder": "    {0}: \n",
+        "start_yield_block": "\n\nYields:\n",
+        "yield_placeholder": "    {0}: {1}\n",
+        "start_raise_block": "\n\nRaises:\n",
+        "raise_placeholder": "    {0}: \n",
+        "start_attributes": "\n\nAttributes:\n",
+        "attribute_placeholder": "    {0} ({1}): {2}\n",
+    },
+    "numpy": {
+        "start_args_block": "\n\n    Parameters\n    ----------\n",
+        "param_placeholder": "    {0} : {1}\n        {2}\n",
+        "param_placeholder_args": "    *{0}\n        {1}\n",
+        "param_placeholder_kwargs": "    **{0}\n        {1}\n",
+        "start_return_block": "\n\n    Returns\n    -------\n",
+        "return_placeholder": "    {0}\n        {1}\n",
+        "return_annotation_placeholder": "    {0}\n        \n",
+        "start_yield_block": "\n\n    Yields\n    ------\n",
+        "yield_placeholder": "    {0}\n        {1}\n",
+        "start_raise_block": "\n\n    Raises\n    ------\n",
+        "raise_placeholder": "    {0}\n        \n",
+        "start_attributes": "\n\n    Attributes\n    ----------\n",
+        "attribute_placeholder": "    {0} : {1}\n        {2}\n",
+    },
+    "reST": {
+        "start_args_block": "\n\n",
+        "param_placeholder": ":param {0}: {2}\n:type {0}: {1}\n",
+        "param_placeholder_args": ":param *{0}: {1}\n",
+        "param_placeholder_kwargs": ":param **{0}: {1}\n",
+        "start_return_block": "\n\n",
+        "return_placeholder": ":return: {1}\n:rtype: {0}\n",
+        "return_annotation_placeholder": ":return: \n:rtype: {0}\n",
+        "start_yield_block": "\n\n",
+        "yield_placeholder": ":yields: {1}\n:ytype: {0}\n",
+        "start_raise_block": "\n\n",
+        "raise_placeholder": ":raises {0}: \n",
+        "start_attributes": "\n\n",
+        "attribute_placeholder": ":var {0}: {2}\n:type {0}: {1}\n",
+    },
+}
+
 
 def generate_docstring(source, position=(1, 0), formatter="google", autocomplete=False):
     """Generate a docstring
@@ -30,11 +81,11 @@ def generate_docstring(source, position=(1, 0), formatter="google", autocomplete
     if autocomplete:
         lines = source.splitlines(True)
         # all full lines before the one the position is on
-        lines_before = lines[:position[0] - 1]
+        lines_before = lines[: position[0] - 1]
         # position in buffer is length of all those lines + the column position (starting at 0)
         bufferpos = sum(len(l) for l in lines_before) + position[1]
         # Splice the desired bits of the source together
-        slice1 = source[:bufferpos - 3]
+        slice1 = source[: bufferpos - 3]
         slice2 = source[bufferpos:]
         source = slice1 + slice2
         # Shift the position to account for the removed quotes
@@ -48,23 +99,25 @@ def generate_docstring(source, position=(1, 0), formatter="google", autocomplete
         leaf = tree
     if not leaf:  # pragma: no cover
         raise exc.FailedToGenerateDocstringError(
-            "Could not find leaf at cursor position {}".format(position))
-    scopes = ('classdef', 'funcdef', 'file_input')
+            "Could not find leaf at cursor position {}".format(position)
+        )
+    scopes = ("classdef", "funcdef", "file_input")
     scope = search_ancestor(leaf, *scopes)
     if not scope:
-        if leaf.type == 'file_input':
+        if leaf.type == "file_input":
             scope = leaf
         else:  # pragma: no cover
             raise exc.FailedToGenerateDocstringError(
-                "Could not find scope of leaf {} ".format(leaf))
+                "Could not find scope of leaf {} ".format(leaf)
+            )
 
-    formatter_module = formatters.get(formatter)
-    if scope.type == 'classdef':
-        return formatter_module.class_docstring(scope)
-    elif scope.type == 'funcdef':
-        return formatter_module.function_docstring(scope)
-    elif scope.type == 'file_input':
-        return formatter_module.module_docstring(scope)
+    if scope.type == "classdef":
+        return pydocstring.formatter.class_docstring(scope, FORMATTER[formatter])
+    elif scope.type == "funcdef":
+        return pydocstring.formatter.function_docstring(scope, FORMATTER[formatter])
+    elif scope.type == "file_input":
+        return pydocstring.formatter.module_docstring(scope, FORMATTER[formatter])
 
     raise exc.FailedToGenerateDocstringError(
-        "Failed to generate Docstring for: {}".format(scope)) # pragma: no cover
+        "Failed to generate Docstring for: {}".format(scope)
+    )  # pragma: no cover
